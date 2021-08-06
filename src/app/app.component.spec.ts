feaaabac0a49682ea41f0ef834866aae8c0523e3
Tino Steinort <tinosteinort@live.de>
2021-08-06 15:14:15 +0200

    remove karma, use jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'picture-story'`, () => {
+  it(`should have as title 'Picture Story'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('picture-story');
+    expect(app.title).toEqual('Picture Story');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('picture-story app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Picture Story');
   });
 });
